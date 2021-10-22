5173b0ab6185f89300bd2befd874dcecefeab1be
ourainbows <casasjuanfelipe@gmail.com>
2021-10-21 23:54:23 -0500

    Creado controlador de calificacion

--- src/main/java/co/edu/usa/fincaapp/api/ClienteController.java
+++ src/main/java/co/edu/usa/fincaapp/api/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,8 @@ import co.edu.usa.fincaapp.servicios.ClienteServicio;
  */
 @RestController
 @RequestMapping("api/Client")
+@CrossOrigin(origins = "*")
+
 public class ClienteController {
     //llamada al servicio
     @Autowired private ClienteServicio clienteServicio;   

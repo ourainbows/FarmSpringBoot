7577e59fb354cbe3b074276fd8ae2f84284ce3b8
ourainbows <casasjuanfelipe@gmail.com>
2021-10-27 22:38:00 -0500

    Agregado al repositorio de reserva los reportes

--- src/main/java/co/edu/usa/fincaapp/repositorios/ReservaCrudRepository.java
+++ src/main/java/co/edu/usa/fincaapp/repositorios/ReservaCrudRepository.java
@@ -1,9 +1,21 @@
 package co.edu.usa.fincaapp.repositorios;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.usa.fincaapp.entidades.Reserva;
 
 public interface ReservaCrudRepository extends CrudRepository <Reserva, Long>  {
     
+    public List<Reserva> findAllByStatus(String status);
+
+    public List<Reserva> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+
+    // select clientid, count(*) as "total" from reservacion group by clientid order
+    // by total desc;
+    @Query("SELECT c.client, COUNT(c.client) FROM Reserva AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationByClient();
 }
--- src/main/java/co/edu/usa/fincaapp/repositorios/ReservaRepository.java
+++ src/main/java/co/edu/usa/fincaapp/repositorios/ReservaRepository.java
@@ -1,12 +1,16 @@
 package co.edu.usa.fincaapp.repositorios;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import co.edu.usa.fincaapp.entidades.Cliente;
 import co.edu.usa.fincaapp.entidades.Reserva;
+import co.edu.usa.fincaapp.reportes.ContadorClientes;
 
 /**
  * Creamos el CRUD
@@ -48,5 +52,22 @@ public class ReservaRepository {
      */
     public void borrarReserva(Reserva reserva) {
         reservaRepositorio.delete(reserva);
+    }
+
+    public List<Reserva> getReservaByStatus(String status) {
+        return reservaRepositorio.findAllByStatus(status);
+    }
+
+    public List<Reserva> getReservaPeriod(Date firstDate, Date secondDate) {
+        return reservaRepositorio.findAllByStartDateAfterAndStartDateBefore(firstDate, secondDate);
+    }
+
+    public List<ContadorClientes> getTopClients() {
+        List<ContadorClientes> res = new ArrayList<>();
+        List<Object[]> report = reservaRepositorio.countTotalReservationByClient();
+        for (int i = 0; i < report.size(); i++) {
+            res.add(new ContadorClientes((Long) report.get(i)[1], (Cliente) report.get(i)[0]));
+        }
+        return res;
     }
 }
